51d81a86f41a9d96184428e989155c49b4ba2835
NadavMozeson <nadavson02@gmail.com>
2023-05-28 13:38:50 +0300

    Finished simple GUI

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-import speech_recognition as sr
-from pabloGUI import openGui, create_icon
-
-def heyPablo():
-
-    def command():
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source, duration=1)
-            print("Listening...")
-            audio = r.listen(source)
-
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language='en-in')
-            print(f"User said: {query}\n")
-        except Exception as e:
-            print("Say that again please...")
-            return "None"
-        return query
-
-    while True:
-        query = command().lower()
-        if 'hey pablo' in query or 'hello pablo' in query:
-            openGui()
+from pabloTray import create_icon
 
 if __name__ == '__main__':
-    #heyPablo()
     create_icon()
--- pabloTray.py
+++ pabloTray.py
@@ -4,7 +4,8 @@ import pystray
 import threading
 import speech_recognition as sr
 import pyttsx3
-import sys
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 
 engine = pyttsx3.init()
 root = None
@@ -23,6 +24,12 @@ def on_exit(icon, item):
     exit_event.set()
     if root:
         root.quit()
+
+def minimize_window(event):
+    root.withdraw()
+
+def unmap_window(event):
+    root.iconify()
 
 def openGui():
     global root
@@ -43,15 +50,33 @@ def openGui():
         engine.say(audio)
         engine.runAndWait()
 
-    root = tk.Tk()
-    frame = tk.Frame(root)
-    frame.pack()
-    button = tk.Button(frame, text="LISTEN", fg="red", command=listen)
-    button.pack(side=tk.LEFT)
+    def send_message():
+        message = entry.get()
+        # Process the message here
+        print(message)
+        entry.delete(0, tk.END)
 
-    root.protocol('WM_DELETE_WINDOW', root.destroy)
-    root.bind("<FocusOut>", lambda event: root.destroy())
-    root.overrideredirect(True)
+    root = tk.Tk()
+    root.title("Pablo - Personal Assistant Bot")
+    frame = ttk.Frame(root, padding="10")
+    frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+    history = ScrolledText(frame, width=30, height=10, wrap=tk.WORD)
+    history.grid(column=0, row=0, columnspan=2, sticky=(tk.W, tk.E))
+    history.config(state='disabled')
+    entry = ttk.Entry(frame, width=20)
+    entry.grid(column=0, row=1, sticky=(tk.W, tk.E))
+    send_button = ttk.Button(frame, text="Send", command=send_message)
+    send_button.grid(column=1, row=1, sticky=tk.W)
+    listen_button = ttk.Button(frame, text="Listen", command=listen)
+    listen_button.grid(column=0, row=2, columnspan=2)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=3)
+    frame.columnconfigure(1, weight=1)
+
+    root.protocol('WM_DELETE_WINDOW', root.withdraw)
+    root.geometry("+{}+{}".format(root.winfo_screenwidth() - root.winfo_reqwidth()-90, 0))
+    root.mainloop()
 
     while True:
         try:
